02120142edfa803e50635e89b45546580f615271
utcool7 <utcool7@gmail.com>
2020-09-27 23:17:18 +0530

    added increase qty function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            price:999,
+            title:'Mobile Phone',
+            qty: 1,
+            img:''
+        }
+    }
+    increaseQuantity = () => {
+        console.log('this',this.state)
+    }
     render(){
+        const {price,title,qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -9,11 +22,27 @@ class CartItem extends React.Component{
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons */}
+                        <img 
+                           alt="increase" 
+                           className="action-icons" 
+                           src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" 
+                           onClick={this.increaseQuantity}
+                        />
+                        <img 
+                           alt="decrease" 
+                           className="action-icons" 
+                           src="https://www.flaticon.com/svg/static/icons/svg/659/659892.svg" 
+                        />
+                        <img 
+                           alt="delete" 
+                           className="action-icons" 
+                           src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg" 
+                         />
                     </div>
                 </div>
             </div>
