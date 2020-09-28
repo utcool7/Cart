32eb539883f3103a23d3c7824e89c55144fa039c
utcool7 <utcool7@gmail.com>
2020-09-28 13:12:17 +0530

    added delete product feature

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,58 +2,14 @@ import React from 'react';
 
 class CartItem extends React.Component{
    
-
-    // testing () {
-
-    //     const promise = new Promise((resolve,reject)=>{
-    //         setTimeout(()=>{
-    //             resolve('done');
-    //         },5000)
-    //     })
-    //     promise.then(()=>{
-    //         //setState acts like a synchronus call
-    //         this.setState({qty:this.state.qty+10});
-    //         this.setState({qty:this.state.qty+10});
-    //         this.setState({qty:this.state.qty+10});
-    //         console.log('state',this.state);
-    //     })
-    // }
-
-    //arrow function directly binds this pointer
-    increaseQuantity = () => {
-        //console.log('this',this.state);
-        //set state rerender our component with updated value 
-        
-        //setState form 1 
-        // this.setState(
-        //     {
-        //         qty : this.state.qty +1 
-        //     }
-        // );
-
-        //setState Form 2
-        this.setState((prevState)=>{
-            return{
-                qty :prevState.qty +1
-            }
-
-        });
-    }
-    decreaseQuantity = () => {
-        
-        const {qty} = this.state;
-        if(qty === 0)
-            return;
-        //setState Form 2
-        this.setState((prevState)=>{
-            return{
-                qty :prevState.qty - 1
-            }
-
-        });
-    }
     render(){
         const {price,title,qty} = this.props.product;
+        const {
+            product,
+            onIncreaseQuantity,
+            onDecreaseQuantity,
+            onDeleteProduct
+        } = this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -70,18 +26,19 @@ class CartItem extends React.Component{
                            alt="increase" 
                            className="action-icons" 
                            src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" 
-                           onClick={this.increaseQuantity}
+                           onClick={()=> onIncreaseQuantity(product)}
                         />
                         <img 
                            alt="decrease" 
                            className="action-icons" 
                            src="https://www.flaticon.com/svg/static/icons/svg/659/659892.svg" 
-                           onClick={this.decreaseQuantity}
+                           onClick={()=> onDecreaseQuantity(product)}
                         />
                         <img 
                            alt="delete" 
                            className="action-icons" 
-                           src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg" 
+                           src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg"
+                           onClick = {()=> onDeleteProduct(product.id)} 
                          />
                     </div>
                 </div>
--- src/cart.js
+++ src/cart.js
@@ -30,6 +30,38 @@ class Cart extends React.Component{
     }
         // this.testing();
     }
+    handleIncreaseQuantity = (product) => {
+        console.log('Heyy please increase the quantity of',product);
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products
+        })
+    }
+    handleDecreaseQuantity = (product) => {
+        console.log('Heyy please decrease the quantity of',product);
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        if(products[index].qty === 0)
+            return;
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        })
+    }
+    handleDeleteProduct = (id) => {
+        const{products} = this.state;
+        // excludes item having id == id 
+
+        const items = products.filter((item) => item.id !== id);
+         this.setState({
+             products: items
+         })
+    }
     render() {
         const {products} = this.state;
 
@@ -39,6 +71,9 @@ class Cart extends React.Component{
                     return (
                         <CartItem product = {product} 
                         key = {product.id}
+                        onIncreaseQuantity={this.handleIncreaseQuantity}
+                        onDecreaseQuantity={this.handleDecreaseQuantity }
+                        onDeleteProduct={this.handleDeleteProduct}
                         />)
                  })}
                  
